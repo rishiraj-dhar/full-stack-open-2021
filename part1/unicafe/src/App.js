9b8cdf02e122018cc4946d3de528978bdf09b01a
rishiraj-dhar <rishiraj1203@gmail.com>
2021-11-28 01:08:39 +0530

    Complete Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,10 +5,32 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  
+  const voteGood = () => {
+    setGood(good + 1);
+  };
+
+  const voteNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const voteBad = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={voteGood}>good</button>
+      <button onClick={voteNeutral}>neutral</button>
+      <button onClick={voteBad}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad)/(good + neutral + bad)}</p>
+      <p>positive {(good/(good + neutral + bad)) * 100} %</p>
     </div>
   )
 };
