731ecc44c69274923046ddaa62ef713b11543e09
rishiraj-dhar <rishiraj1203@gmail.com>
2021-11-28 12:01:55 +0530

    Complete Exercise 2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  let total = 0;
+  parts.forEach(part => {
+    total += part.exercises;
+  });
+  return (
+    <p>
+      Number of exercises {total}
+    </p>
+  );
+};
+
+const Course = ({ course }) => (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      {/* <Total parts={course.parts} /> */}
+    </div>
+);
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  };
+
+  return <Course course={course} />
+};
+
+export default App;
