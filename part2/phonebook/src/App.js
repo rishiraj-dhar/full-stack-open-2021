17b87ef7712e74362fe66a263847cef999db29e2
rishiraj-dhar <rishiraj1203@gmail.com>
2021-11-28 19:49:04 +0530

    Complete Exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,40 @@
 import React, { useState } from 'react';
 
+const TextInput = ({ labelText, value, handleInput }) => {
+  return (
+    <div>
+        {labelText} <input onChange={handleInput} value={value} />
+    </div>
+  );
+};
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <div>
+      <button onClick={handleClick} type="submit">{text}</button>
+    </div>
+  );
+};
+
+const FilteredPersons = ({persons, nameFilter}) => {
+  const personsToShow = persons.filter(person => (
+      person.name.toLowerCase()
+      .includes(nameFilter.toLowerCase())
+      ));
+  
+  return (
+    <>
+      {personsToShow.map(
+        person => (
+          <p key={person.id}>
+            {person.name} {person.number}
+          </p>
+        ))}
+    </>
+  );
+
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -34,40 +69,26 @@ const App = () => {
     const newPersons = persons.concat(
       {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length + 1
       });
     setPersons(newPersons);
     setNewName('');
     setNewNumber('');
   };
 
-  const personsToShow = () => {
-    return persons.filter(person => (
-      person.name.toLowerCase()
-      .includes(nameFilter.toLowerCase())
-      ));
-  };
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input onChange={handleNameFilterInput} value={nameFilter} />
-      </div>
+      <TextInput labelText="filter shown with " handleInput={handleNameFilterInput} value={nameFilter} />
       <h2>add a new</h2>
       <form>
-        <div>
-          name: <input onChange={handleNameInput} value={newName} />
-        </div>
-        <div>
-          number: <input onChange={handleNumberInput} value={newNumber} />
-        </div>
-        <div>
-          <button onClick={handleAddPerson} type="submit">add</button>
-        </div>
+        <TextInput labelText="name: " value={newName} handleInput={handleNameInput} />
+        <TextInput labelText="number: " value={newNumber} handleInput={handleNumberInput} />
+        <Button handleClick={handleAddPerson} text="add" />
       </form>
       <h2>Numbers</h2>
-      {personsToShow().map(person => <p key={person.id}>{person.name} {person.number}</p>)}
+      <FilteredPersons persons={persons} nameFilter={nameFilter} />
     </div>
   );
 };
