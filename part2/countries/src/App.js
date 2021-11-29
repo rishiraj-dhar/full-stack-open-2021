2ccaf71b90775e2cfb8a3a612e0a77d7f3fd1083
rishiraj-dhar <rishiraj1203@gmail.com>
2021-11-29 14:41:08 +0530

    Complete Exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,65 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const WeatherInfo = ({ countryData }) => {
-  const API_key = process.env.REACT_APP_OW_API_KEY;
-  const [weatherData, setWeatherData] = useState({});
-
-  const windDirection = (deg) => {
-    if ((deg >= 348.75 && deg < 360) || (deg >= 0 && deg < 11.25)) {
-      return 'N';
-    } else if (deg >= 11.25 && deg < 33.75) {
-      return 'NNE';
-    } else if (deg >= 33.75 && deg < 56.25) {
-      return 'NE';
-    } else if (deg >= 56.25 && deg < 78.75) {
-      return 'ENE';
-    } else if (deg >= 78.75 && deg < 101.25) {
-      return 'E';
-    } else if (deg >= 101.25 && deg < 123.75) {
-      return 'ESE';
-    } else if (deg >= 123.75 && deg < 146.25) {
-      return 'SE';
-    } else if (deg >= 146.25 && deg < 168.75) {
-      return 'SSE';
-    } else if (deg >= 168.75 && deg < 191.25) {
-      return 'S';
-    } else if (deg >= 191.25 && deg < 213.75) {
-      return 'SSW';
-    } else if (deg >= 213.75 && deg < 236.25) {
-      return 'SW';
-    } else if (deg >= 236.25 && deg < 258.75) {
-      return 'WSW';
-    } else if (deg >= 258.75 && deg < 281.25) {
-      return 'W';
-    } else if (deg >= 281.25 && deg < 303.75) {
-      return 'WNW';
-    } else if (deg >= 303.75 && deg < 326.25) {
-      return 'NW';
-    } else if (deg >= 326.25 && deg < 348.75) {
-      return 'NNW';
-    } else {
-      return null;
-    }
-  };
+const WeatherInfo = ({ city }) => {
+  const [ weatherData, setWeatherData ] = useState(null);
 
   useEffect(() => {
+    const API_key = process.env.REACT_APP_WEATHER_API_KEY;
     axios
-      .get(`api.openweathermap.org/data/2.5/weather?q=${countryData.capital}&appid=${API_key}&units=metric`)
+      .get(`http://api.weatherapi.com/v1/current.json?key=${API_key}&q=${city}&aqi=no`)
       .then(response => {
-        setWeatherData(response.data);
-        return (
+        setWeatherData(
           <div>
-            <h3>Weather in {countryData.name.common}</h3>
-            <p><strong>temperature:</strong> {weatherData.main.temp}°C</p>
-            <img src={`https://openweathermap.org/img/wn/${weatherData.weather.icon}.png`} alt={"weather icon"} />
-            <p><strong>wind:</strong> {weatherData.wind.speed} m/s direction {windDirection(weatherData.wind.deg)}</p>
+            <h3>Weather in {city}</h3>
+            <p><strong>temperature:</strong> {response.data.current.temp_c}°C</p>
+            <img src={response.data.current.condition.icon} alt={"weather icon"} />
+            <p><strong>wind:</strong> {response.data.current.wind_mph} mph direction {response.data.current.wind_dir}</p>
           </div>
         );
       })
   }, []);
 
-  return null;
+  return weatherData;
 
 };
 
@@ -74,7 +35,7 @@ const CountryInfo = ({ countryData }) => {
         {Object.values(countryData.languages).map(language => <li key={language} >{language}</li> )}
       </ul>
       <img src={countryData.flags.png} alt={`Flag of ${countryData.name.common}`} />
-      <WeatherInfo countryData={countryData} />
+      <WeatherInfo city={countryData.capital} />
     </div>
   );
 };
